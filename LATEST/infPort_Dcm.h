ea9993b86405184e4b563098a6e132081c2e5278
root <nagaraja.hm.0001@gmail.com>
2022-03-11 12:12:44 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Port

--- LATEST/infPort_Dcm.h
+++ LATEST/infPort_Dcm.h
@@ -1,15 +1,13 @@
+#pragma once
 /*****************************************************/
-/* File   : Port.cpp                                 */
+/* File   : infPort_Dcm.h                            */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "module.h"
-#include "infPort_EcuM.h"
-#include "infPort_Dcm.h"
-#include "infPort_SchM.h"
+#include "Compiler_Cfg_Port.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -22,15 +20,6 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
-class module_Port:
-      public abstract_module
-{
-   public:
-      FUNC(void, PORT_CODE) InitFunction   (void);
-      FUNC(void, PORT_CODE) DeInitFunction (void);
-      FUNC(void, PORT_CODE) GetVersionInfo (void);
-      FUNC(void, PORT_CODE) MainFunction   (void);
-};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -43,36 +32,11 @@ class module_Port:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Port    Port;
-infEcuMClient* gptrinfEcuMClient_Port = &Port;
-infDcmClient*  gptrinfDcmClient_Port  = &Port;
-infSchMClient* gptrinfSchMClient_Port = &Port;
+extern infDcmClient* gptrinfDcmClient_Port;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, PORT_CODE) module_Port::InitFunction(void){
-}
-
-FUNC(void, PORT_CODE) module_Port::DeInitFunction(void){
-}
-
-FUNC(void, PORT_CODE) module_Port::GetVersionInfo(void){
-}
-
-FUNC(void, PORT_CODE) module_Port::MainFunction(void){
-}
-
-#include "Port_Unused.h"
-
-FUNC(void, PORT_CODE) class_Port_Unused::SetPinDirection(void){
-}
-
-FUNC(void, PORT_CODE) class_Port_Unused::RefreshPortDirection(void){
-}
-
-FUNC(void, PORT_CODE) class_Port_Unused::SetPortMode(void){
-}
 
 /*****************************************************/
 /* EOF                                               */
