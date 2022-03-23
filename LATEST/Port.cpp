b484c9bcc29be0abdf3dfaf4363980e3de3b4196
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:08:00 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Port

--- LATEST/Port.cpp
+++ LATEST/Port.cpp
@@ -40,6 +40,16 @@ class module_Port:
       FUNC(void, PORT_CODE) DeInitFunction (void);
       FUNC(void, PORT_CODE) GetVersionInfo (void);
       FUNC(void, PORT_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, PORT_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
