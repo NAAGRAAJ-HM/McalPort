bd6916bc83d7896e3129de1df798cb6a8aba5479
root <nagaraja.hm.0001@gmail.com>
2022-03-31 12:04:16 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Port

--- LATEST/Port.cpp
+++ LATEST/Port.cpp
@@ -37,10 +37,9 @@ class module_Port:
    public:
       module_Port(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
-      FUNC(void, _CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      FUNC(void, PORT_CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, PORT_CONFIG_DATA, PORT_APPL_CONST) lptrCfgModule
       );
-      FUNC(void, PORT_CODE) InitFunction   (void);
       FUNC(void, PORT_CODE) DeInitFunction (void);
       FUNC(void, PORT_CODE) MainFunction   (void);
 };
@@ -77,23 +76,39 @@ VAR(module_Port, PORT_VAR) Port(
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
 FUNC(void, PORT_CODE) module_Port::InitFunction(
-   CONSTP2CONST(CfgPort_Type, CFGPORT_CONFIG_DATA, CFGPORT_APPL_CONST) lptrCfgPort
+   CONSTP2CONST(CfgModule_TypeAbstract, PORT_CONFIG_DATA, PORT_APPL_CONST) lptrCfgModule
 ){
-   if(NULL_PTR == lptrCfgPort){
+   if(E_OK == IsInitDone){
 #if(STD_ON == Port_DevErrorDetect)
       Det_ReportError(
       );
 #endif
    }
    else{
-// check lptrCfgPort for memory faults
+      if(NULL_PTR == lptrCfgModule){
+#if(STD_ON == Port_DevErrorDetect)
+         Det_ReportError(
+         );
+#endif
+      }
+      else{
+// check lptrCfgModule for memory faults
 // use PBcfg_Port as back-up configuration
+      }
+      IsInitDone = E_OK;
    }
-   Port.IsInitDone = E_OK;
 }
 
 FUNC(void, PORT_CODE) module_Port::DeInitFunction(void){
-   Port.IsInitDone = E_NOT_OK;
+   if(E_OK != IsInitDone){
+#if(STD_ON == Port_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+      IsInitDone = E_NOT_OK;
+   }
 }
 
 FUNC(void, PORT_CODE) module_Port::MainFunction(void){
