b7f532a587995da8be51ebbaee30aff7d2b5dc08
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:25:07 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Port

--- LATEST/Port.cpp
+++ LATEST/Port.cpp
@@ -101,8 +101,8 @@ FUNC(void, PORT_CODE) module_Port::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+            lptrCfg = &PBcfgPort;
          }
       }
       IsInitDone = E_OK;
