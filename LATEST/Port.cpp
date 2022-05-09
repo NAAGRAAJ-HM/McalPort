dca165f385c82e836fa401a07f5a564e5638ccd9
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:35:43 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Port

--- LATEST/Port.cpp
+++ LATEST/Port.cpp
@@ -35,6 +35,13 @@ class module_Port:
       public abstract_module
    ,  public class_Port_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Port(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
