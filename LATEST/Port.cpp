ddeb63f8e3365d1c5b652975ab8e35207a4ad2db
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-02 12:06:54 +0200

    Update: AUTOSAR micro-controller abstraction submodule - Port

--- LATEST/Port.cpp
+++ LATEST/Port.cpp
@@ -72,6 +72,10 @@ CONSTP2VAR(infSchMClient, PORT_VAR, PORT_CONST) gptrinfSchMClient_Port = &Port;
 /******************************************************************************/
 VAR(module_Port, PORT_VAR) Port(
    {
+#if(STD_ON == _ReSIM)
+// char strModuleName[6];
+#else
+#endif
          PORT_AR_RELEASE_VERSION_MAJOR
       ,  PORT_AR_RELEASE_VERSION_MINOR
       ,  0x00
