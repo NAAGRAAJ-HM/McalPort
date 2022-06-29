b5153cb258a38ef347eee51f050d213de35810d9
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-29 09:02:55 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module for port

--- LATEST/Port.cpp
+++ LATEST/Port.cpp
@@ -41,12 +41,8 @@ class module_Port:
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-            Std_TypeReturn          IsInitDone{E_NOT_OK};
-      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
 
    public:
-      module_Port(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
-      }
       FUNC(void, PORT_CODE) InitFunction(
          CONSTP2CONST(CfgModule_TypeAbstract, PORT_CONFIG_DATA, PORT_APPL_CONST) lptrCfgModule
       );
@@ -71,18 +67,7 @@ CONSTP2VAR(infSchMClient, PORT_VAR, PORT_CONST) gptrinfSchMClient_Port = &Port;
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-VAR(module_Port, PORT_VAR) Port(
-   {
-         PORT_AR_RELEASE_VERSION_MAJOR
-      ,  PORT_AR_RELEASE_VERSION_MINOR
-      ,  0x00
-      ,  0xFF
-      ,  0x01
-      ,  '0'
-      ,  '1'
-      ,  '0'
-   }
-);
+VAR(module_Port, PORT_VAR) Port;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
